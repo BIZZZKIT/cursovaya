31314e501c2142c858685370ea3e61e67b7e652b
Rozen Maxim <max.r2005@gmail.com>
2025-01-16 20:18:37 +0500

    Бутстрап подключен, добавлен в welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'ТехноДLом')</title>
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
+    <script src="{{asset('assets/js/bootstrap.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.bundle.js')}}"></script>
+    <title>@yield('title', 'ТехноДом')</title>
 </head>
 <body>
 
