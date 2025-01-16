74cdf332b4d17bfc819afe2ae90d515138cf0463
Rozen Maxim <max.r2005@gmail.com>
2025-01-17 01:16:31 +0500

    Добавление категории

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function createCategory()
+    public function createCategory(Request $request)
     {
-
+        $name_category = $request->input('name_category');
+        Category::create([
+            'name_category' => $name_category,
+        ]);
+        return back()->with(['succesCreateCategory' => 'Успешно создали категорию']);
     }
 }
