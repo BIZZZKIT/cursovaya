1c62f52f6ff042527bfd09cea306bd30cea23737
YSL-23 <krymmistik801@gmail.com>
2025-01-16 22:11:29 +0500

    Кнопки хеадер

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,32 +1,40 @@
+@php use Illuminate\Support\Facades\Auth; @endphp
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container d-flex align-items-center">
-        <a href="{{route('main')}}"><img src="{{asset('assets/images/logo.png')}}" height="50px" alt="" class="me-3"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a href="{{route('main')}}"><img src="{{asset('assets/images/logo.png')}}" height="50px" alt=""
+                                         class="me-3"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="#">Главная</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('about')}}">Каталог</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('about')}}">О нас</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
             </ul>
+            <div class="d-flex gap-3">
+                @auth()
+                    @if(Auth::user()->isAdmin)
+                        <a href="{{ route('adminPanel') }}" class="btn text-white btn-info">Панель админа</a>
+                    @endif
+                    @if(!Auth::user()->isAdmin)
+                            <a href="{{ route('add_order') }}" class="btn btn-primary">Корзина</a>
+                    @endif
+                    <a href="{{ route('logout') }}" class="btn btn-danger">Выйти</a>
+                @endauth
+
+                @guest()
+                    <div class="btn btn-primary">Вход</div>
+                    <div class="btn text-white btn-info">Зарегистрироваться</div>
+                @endguest
+            </div>
         </div>
     </div>
 </nav>
